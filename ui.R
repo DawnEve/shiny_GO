3392f9ebae31d8e09f7df177e6e6acc3fcf936ea
jimmy <JimmyMall@live.com>
2020-07-16 14:16:49 +0800

    v1.0.5 mv demo btn to the top

--- ui.R
+++ ui.R
@@ -14,19 +14,17 @@ shinyUI(fluidPage(
   
   # 侧面控件 Sidebar with a slider input for number of observations
   sidebarPanel(
-    selectInput("species", h3("Select species(暂不支持小鼠)"), 
+    selectInput("species", h3("Step1: Select species(暂不支持小鼠)"), 
                        choices = c("human", "mouse"), selected = 'human'),
-    textAreaInput("genes", h3("Input gene list"), 
+    # 填充示例基因
+    h3("Step2: Input gene list", HTML("<a id='use_demo' href='javascript:void(0);'>Demo genes</a>")),
+    textAreaInput("genes", "", 
                      placeholder  = "Please input gene symbols(at lest >5), seperated by blank, tab or one symbol per line.", 
                      rows  =10),
-    actionButton("use_demo", 'Demo genes'), # 填充示例基因
+    HTML("<p class=grey>Input gene number: <span id='geneNumber'></span></p>"),
 
+    h3("Step3: Submit"),
     actionButton("submit", "Submit"), #提交按钮
-
-    hr(),
-    #span('Input gene number: '),
-    #textOutput('geneNumber', inline=T)
-    HTML("<p>Input gene number: <span id='geneNumber'></span></p>")
   ),
   
   # 右边 主控件(默认右边) Show a plot of the generated distribution
--- www/my.js
+++ www/my.js
@@ -33,8 +33,8 @@ function trim(str){
 addEvent(window,'load',function(){
 	var $2=function(o){return document.getElementById(o);}
 
-	//提交按钮点击时，统计数量
-	$2('submit').onclick=function(){
+	//统计基因数量
+	var countGeneNumber=function(){
 		// alert('you clicked me!')
 		var text=trim( $2('genes').value );
 		var arr=text.split(/[^0-9a-zA-Z\-\.]+/g)
@@ -43,9 +43,12 @@ addEvent(window,'load',function(){
 		//console.log(arr)
 		//alert('Begin enrichment now, please waite.')
 	}
+	//提交按钮点击时，统计基因数量
+	$2('genes').onchange=countGeneNumber
+	$2('genes').onkeyup=countGeneNumber //抬起来键盘时
 
 	//输入框: 单击时全选
-	$2('genes').onclick=function(){
+	$2('genes').onmouseover=function(){
 		this.select()
 	}
 
@@ -53,10 +56,12 @@ addEvent(window,'load',function(){
 	$2('use_demo').onclick=function(){
 		var text_box=$2('genes')
 		var glist="ASF1B ATAD2 BLM BRCA1 CALD1 CALM2 CCDC14 CCDC84 CDCA5 CENPM CHML DCAF16 DHFR DNAJB4 DONSON DSCC1 DYNC1LI2 EIF4EBP2 ENOSF1 ESCO2 EXO1 EZH2 FANCI GCLM HELLS HIST1H4C MAP3K2 MASTL MCM8 MYCBP2 NEAT1 NFE2L2 NSUN3 NT5DC1 OGT ORC3 PHIP PHTF2 PKMYT1 POLA1 PRIM1 PTAR1 RAD18 RBBP8 RFC2 RRM1 RRM2 RSRC2 SAP30BP SLC38A2 SRSF5 SVIP TOP2A TYMS UBE2T USP1 ZWINT"
-		// $2('genes').value=glist;
-		text_box.innerHTML=glist
+		text_box.value=glist; //.value属性比.innerHTML好用，特别是被反复修改后
+		//text_box.innerHTML=glist
 		// 要主动触发输入框的改变事件
 		trigger(text_box,'change')
+
+		//console.log('you clicked me: demo')
 	}
 
 });
